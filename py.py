c7ba3741bdc263bc3b16ead49d2755b8189bcc1d
Ultimate333 <belov.evgenijj333@gmail.com>
2025-10-26 19:00:41 +0300

    игра угадай число

--- py.py
+++ py.py
@@ -1,4 +1,12 @@
-a = 1
-b = 2
-c = a + b
-print(c)
+import random
+
+try:
+    while True:
+        secret_number = random.randint(1, 10)
+        while int(input("Угадайте число от 1 до 10: ")) != secret_number:
+            print("не угадали")
+        print("верно!")
+except ValueError:
+    print("Ошибка: введите число!")
+except Exception as e:
+    print(f"Ошибка: {str(e)}")
